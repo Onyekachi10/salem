0fd94202bcdd09ba3cec29b9455dac153f851135
Eguzozie Onyekachi <eguzozieonyekachi@gmail.com>
2024-10-11 18:06:34 +0100

    prices

--- src/component/grid.jsx
+++ src/component/grid.jsx
@@ -52,6 +52,7 @@ const Grid = () => {
          type: "kit_7",
          image: "https://photo-gold.vercel.app/images/salem2.jpg",
          title: "Holloween Joker Mask",
+         price: "$25",
          content: "quality Merch"
         },
      
@@ -60,6 +61,14 @@ const Grid = () => {
          image: "https://photo-gold.vercel.app/images/salem1.jpg",
          title: "Half burnt clown",
          content: "quality Merch"
+        },
+
+        {
+            type: "kit_9",
+            image: "https://photo-gold.vercel.app/images/salem6.jpg",
+            title: "Half burnt clown",
+            price: "$47",
+            content: "quality Merch"
         }
      ]
 
@@ -67,12 +76,13 @@ const Grid = () => {
         <div className="pt-16 font-customFont">
     <h1 className="text-center font-bold md:text-5xl text-3xl uppercase pb-3 ">Dark Art</h1>
     <div className="grid md:grid-cols-4 grid-cols-1 gap-8 px-6 py-4">
-    {merch.map(({type, image, title, content})=>(
+    {merch.map(({type, image, title, content,price})=>(
         <div key={type}>
             <div className=" py-4 px-2 border-none focus:outline-none bg-gray-200 text-black
              hover:shadow-md cursor-pointer rounded-lg ">
             <img src={image} className=" object-contain h-fit rounded-md"/>
             <p className="font-semibold capitalize md:text-xl pt-3">{title}</p>
+              <span>{price}</span>
             <span className="font-medium capitalize text-lg">{content}</span>
             </div>
          
